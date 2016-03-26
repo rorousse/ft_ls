d93765d59ee2ec7156b24ee0ef8283d91d541def
Romain ROUSSEL <rorousse@e3r10p2.42.fr>
2016-03-26 17:31:35 +0100

    normay

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: rorousse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/16 13:21:45 by rorousse          #+#    #+#             */
-/*   Updated: 2016/03/26 16:33:55 by rorousse         ###   ########.fr       */
+/*   Updated: 2016/03/26 17:28:41 by rorousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ typedef struct dirent dirent;
 typedef struct passwd passwd;
 typedef struct group group;
 
-struct  taille_max_s
+struct  build_s
 {
     unsigned int nblinks;
     unsigned int username;
@@ -37,7 +37,7 @@ struct  taille_max_s
 	int			color;
 };
 
-typedef struct taille_max_s t_taille_max;
+typedef struct build_s t_build;
 
 struct	file_list_s
 {
@@ -68,9 +68,15 @@ typedef struct dirent_extended_s t_dirent_extended;
 ** AFFICHAGE_C
 */
 
-void		inv_aff_list(t_file_list *lst, t_taille_max taillemax);
-void		aff_list(t_file_list *lst, t_taille_max taillemax);
-void		aff_extended(t_file_list *lst, t_taille_max taillemax);
+void		aff_list(t_file_list *lst, t_build taillemax);
+void		aff_extended(t_file_list *lst, t_build taillemax);
+
+/*
+** BUILD_C
+*/
+
+void		init_build(t_build *build);
+void		define_sizes_build(t_file_list *lst, t_build *build);
 
 /*
 ** DATE_C
@@ -94,10 +100,10 @@ int			ft_ls(int argc, char **argv, char *path);
 ** LISTING_C
 */
 
-t_file_list	*new_elem(dirent *mydirent, char *path, t_taille_max *taillemax);
+t_file_list	*new_elem(dirent *mydirent, char *path, t_build *taillemax);
 void		free_list(t_file_list *lst);
-void		list_add_elem(t_file_list **lst, t_dirent_extended lecture, t_taille_max *taillemax);
-t_file_list	*fill_list(char *path, int hidden, int mode, t_taille_max *taillemax);
+void		list_add_elem(t_file_list **lst, t_dirent_extended lecture, t_build *taillemax);
+t_file_list	*fill_list(char *path, int hidden, int mode, t_build *taillemax);
 void		reverse_list(t_file_list *lst);
 
 /*
@@ -112,7 +118,7 @@ char		*create_path(char *path, char *name);
 
 void		print_typage(mode_t st_mode);
 void		print_rights(mode_t st_mode);
-void		print_name(t_file_list *lst, t_taille_max taillemax);
+void		print_name(t_file_list *lst, t_build taillemax);
 void		print_total_blocks(t_file_list *lst);
 
 /*
